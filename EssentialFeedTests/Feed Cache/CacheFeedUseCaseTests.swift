3e58dbf83f70aa580848a1e61d29d34083a45cc3
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2023-11-01 16:36:49 +0100

    move feedStoreSpy to its own file

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -126,46 +126,6 @@ final class CacheFeedUseCaseTests: XCTestCase {
         return(sut, store)
     }
     
-    private class FeedStoreSpy: FeedStore {
-        typealias DeletionCompletion = (Error?) -> Void
-        typealias InsertionCompletion = (Error?) -> Void
-
-        enum ReceivedMessage: Equatable {
-            case deleteCachedFeed
-            case insert([LocalFeedImage], Date)
-        }
-        
-        private var deletionCompletions = [DeletionCompletion]()
-        private var insertionCompletions = [DeletionCompletion]()
-        private(set) var receivedMesages = [ReceivedMessage]()
-        
-        func deleteCaheFeed(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMesages.append(.deleteCachedFeed)
-        }
-        
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMesages.append(.insert(feed, timestamp))
-        }
-        
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
-    
     private func expect(_ sut: LocalFeedLoader, toCompleteWithError expectedError: NSError?, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "wait for save completion")
         
