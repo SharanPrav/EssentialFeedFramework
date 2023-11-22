12f000a21c38d1bd001f2b6081f6057fa87d56cd
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2023-11-22 16:34:38 +0100

    grouped feedstorespecs and xctesthelpers into one group

---
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpecs/XCTestCase+FailableRetrieveFeedStoreSpecs.swift
@@ -0,0 +1,19 @@
+//
+//  XCTestCase+FailableRetrieveFeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Saranya Ravi on 22/11/2023.
+//
+
+import XCTest
+import EssentialFeed
+
+extension FailableRetrieveFeedStoreSpecs where Self: XCTestCase {
+    func assertThatRetrieveDeliversFailureOnRetrievalError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieve: .failure(anyError()), file: file, line: line)
+    }
+
+    func assertThatRetrieveHasNoSideEffectsOnFailure(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieveTwice: .failure(anyError()), file: file, line: line)
+    }
+}
