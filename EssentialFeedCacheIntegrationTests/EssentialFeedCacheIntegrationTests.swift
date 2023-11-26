59b87a765d64b5b214c7459a348b0d7770e0b1f7
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2023-11-26 21:38:41 +0100

    created a intergration test target for cache module

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Saranya Ravi on 26/11/2023.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
