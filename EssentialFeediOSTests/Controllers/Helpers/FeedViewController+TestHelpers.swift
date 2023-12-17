8162344f03b2860622ae997a83c8ac0b8c07eb84
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2023-12-17 20:39:04 +0100

    Extract FeedImageCell creation/configuration and image loading logic with `FeedImageDataLoader` to the new `FeedImageCellController`

--- EssentialFeediOSTests/Controllers/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeediOSTests/Controllers/Helpers/FeedViewController+TestHelpers.swift
@@ -6,18 +6,6 @@ extension FeedViewController {
         refreshControl?.simulatePullToRefresh()
     }
     
-//    func replaceRefreshControlWithFakeForiOS17Support() {
-//        let fake = FakeRefreshControl()
-//        
-//        refreshControl?.allTargets.forEach { target in
-//            refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in     
-//                fake.addTarget(target, action: Selector(action), for: .valueChanged)
-//            }
-//        }
-//        
-//        refreshControl = fake
-//    }
-    
     func simulateAppearance() {
         if !isViewLoaded {
             refreshController?.replaceRefreshControlWithFakeForiOS17Support()
@@ -74,18 +62,4 @@ extension FeedViewController {
     }
 }
 
-//private class FakeRefreshControl: UIRefreshControl {
-//    private var _isRefreshing = false
-//    override var isRefreshing: Bool {
-//        _isRefreshing
-//    }
-//    
-//    override func beginRefreshing() {
-//        _isRefreshing = true
-//    }
-//    
-//    override func endRefreshing() {
-//        _isRefreshing = false
-//    }
-//}
 
