7f0b3774f9c159baf6e79e6a8ed8ce9e8a74427f
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2023-12-17 16:50:29 +0100

    Extract FeedImageCell test helpers to a separate file

---
+++ EssentialFeediOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+extension FeedImageCell {
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+    
+    var locationText: String? {
+        return locationLabel.text
+    }
+    
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+    
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+    
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+    
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+    
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+}
