78c52d160280e1428c97383c12dca51b498d6e99
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2023-12-20 23:40:25 +0100

    Move FeedLoader loading state management to FeedViewModel. Now, the `FeedRefreshViewController` acts as a binder between the `View` and the `ViewModel`

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -1,32 +1,56 @@
 import UIKit
 import EssentialFeed
 
-final class FeedRefreshViewController: NSObject {
-    
-    // It is supposed to be private(Set) but removed it because it has to be replaced with fake for testing. 
-    lazy var view: UIRefreshControl = {
-        let view = UIRefreshControl()
-        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        return view
-    }()
-    
+final class FeedViewModel {
     private let feedLoader: FeedLoader
     
     init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
     }
     
-    var onRefresh: (([FeedImage]) -> Void)?
+    var onChange: ((FeedViewModel) -> Void)?
+    var onFeedLoad: (([FeedImage]) -> Void)?
+
+    private(set) var isLoading: Bool = false {
+        didSet { onChange?(self) }
+    }
     
-    @objc func refresh() {
-        view.beginRefreshing()
-        
+    func loadFeed() {
+        isLoading = true
         feedLoader.load { [weak self] result in
             if let feed = try? result.get() {
-                self?.onRefresh?(feed)
+                self?.onFeedLoad?(feed)
             }
-            self?.view.endRefreshing()
+            self?.isLoading = false
         }
+    }
+}
+
+final class FeedRefreshViewController: NSObject {
+    
+    // It is supposed to be private(Set) but removed it because it has to be replaced with fake for testing. 
+    lazy var view = binded(UIRefreshControl())
+    
+    private let viewModel: FeedViewModel
+
+    init(viewModel: FeedViewModel) {
+        self.viewModel = viewModel
+    }
+        
+    @objc func refresh() {
+        viewModel.loadFeed()
+    }
+    
+    private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
+        viewModel.onChange = { [weak self] viewModel in
+            if viewModel.isLoading {
+                self?.view.beginRefreshing()
+            } else {
+                self?.view.endRefreshing()
+            }
+        }
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
     }
 }
 
