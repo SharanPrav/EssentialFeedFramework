68b8575be3870e708c573a57e6cd8feeab65c337
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2023-12-21 22:55:59 +0100

    Remove mutable state from FeedViewModel. The FeedViewModel only needs to forward state changes, so state is only transient.

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -2,26 +2,31 @@ import UIKit
 import EssentialFeed
 
 final class FeedViewModel {
+    typealias Observer<T> = (T) -> Void
+
     private let feedLoader: FeedLoader
     
     init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
     }
     
-    var onChange: ((FeedViewModel) -> Void)?
-    var onFeedLoad: (([FeedImage]) -> Void)?
+   // var onChange: ((FeedViewModel) -> Void)?
+   // var onFeedLoad: (([FeedImage]) -> Void)?
 
-    private(set) var isLoading: Bool = false {
-        didSet { onChange?(self) }
-    }
+//    private(set) var isLoading: Bool = false {
+//        didSet { onChange?(self) }
+//    }
+    
+    var onLoadingStateChange: Observer<Bool>?
+    var onFeedLoad: Observer<[FeedImage]>?
     
     func loadFeed() {
-        isLoading = true
+        onLoadingStateChange?(true)
         feedLoader.load { [weak self] result in
             if let feed = try? result.get() {
                 self?.onFeedLoad?(feed)
             }
-            self?.isLoading = false
+            self?.onLoadingStateChange?(false)
         }
     }
 }
@@ -42,13 +47,14 @@ final class FeedRefreshViewController: NSObject {
     }
     
     private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
-        viewModel.onChange = { [weak self] viewModel in
-            if viewModel.isLoading {
+        viewModel.onLoadingStateChange = { [weak self] isLoading in
+            if isLoading {
                 self?.view.beginRefreshing()
             } else {
                 self?.view.endRefreshing()
             }
         }
+        
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
         return view
     }
