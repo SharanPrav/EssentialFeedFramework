b5b99d1ba37f25680dfc199df74c985bcbd77c5f
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-01-20 22:45:31 +0100

    Unify FeedViewController static factory helper into the `FeedUIComposer'

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -3,30 +3,28 @@ import EssentialFeed
 
 public final class FeedUIComposer {
     private init() {}
-
+    
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-//        let feedViewModel = FeedViewModel(feedLoader: feedLoader)
-//        let refreshController = FeedRefreshViewController(viewModel: feedViewModel)
+        //        let feedViewModel = FeedViewModel(feedLoader: feedLoader)
+        //        let refreshController = FeedRefreshViewController(viewModel: feedViewModel)
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
-                    MainQueueDispatchDecorator(decoratee: feedLoader))
+                                                                    MainQueueDispatchDecorator(decoratee: feedLoader))
         
-        let feedController = FeedViewController.makeWith(
-                    delegate: presentationAdapter,
-                    title: FeedPresenter.title)
-
+        let feedController = makeFeedViewController(
+            delegate: presentationAdapter,
+            title: FeedPresenter.title)
+        
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
                 controller: feedController,
-                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),            
+                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
             loadingView: WeakRefVirtualProxy(feedController))
-
-//        feedViewModel.onFeedLoad = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+        
+        //        feedViewModel.onFeedLoad = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
         return feedController
     }
-}
-
-private extension FeedViewController {
-    static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+    
+    private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
