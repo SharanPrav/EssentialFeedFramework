679db4040aeac7d30731943d962fba9eed1c219b
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-01-24 17:24:13 +0100

    moved view models to its own file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Saranya Ravi on 24/01/2024.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Saranya Ravi on 24/01/2024.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
