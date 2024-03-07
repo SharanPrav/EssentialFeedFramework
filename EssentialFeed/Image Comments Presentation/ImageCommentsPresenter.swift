57b717ab8272725b1d6dc77ef801fcba0112dbdf
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-03-07 15:26:29 +0100

    Add type aliases to shorten type definitions with generics

---
+++ EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+            tableName: "ImageComments",
+            bundle: Bundle(for: Self.self),
+            comment: "Title for the image comments view")
+    }
+}
