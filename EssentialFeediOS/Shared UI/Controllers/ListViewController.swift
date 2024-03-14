bf40b332e44d027785655395801979e9588ad4ac
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-03-14 15:15:39 +0100

    made errorview programmatic

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -3,7 +3,7 @@ import EssentialFeed
 
 public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
     
-    @IBOutlet private(set) public var errorView: ErrorView!
+    private(set) public var errorView = ErrorView()
 
     private var loadingControllers = [IndexPath: CellController]()
 
@@ -18,6 +18,8 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
 
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureErrorView()
         
         onViewIsAppearing = { vc in
             vc.onViewIsAppearing = nil
@@ -37,6 +39,28 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         onViewIsAppearing?(self)
     }
     
+    private func configureErrorView() {
+        let container = UIView()
+        container.backgroundColor = .clear
+        container.addSubview(errorView)
+        
+        errorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            errorView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: errorView.trailingAnchor),
+            errorView.topAnchor.constraint(equalTo: container.topAnchor),
+            container.bottomAnchor.constraint(equalTo: errorView.bottomAnchor),
+        ])
+        
+        tableView.tableHeaderView = container
+        
+        errorView.onHide = { [weak self] in
+            self?.tableView.beginUpdates()
+            self?.tableView.sizeTableHeaderToFit()
+            self?.tableView.endUpdates()
+        }
+    }
+    
     public func display(_ cellControllers: [CellController]) {
         tableModel = cellControllers
     }
@@ -47,7 +71,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     }
     
     public func display(_ viewModel: ResourceErrorViewModel) {
-        errorView?.message = viewModel.message
+        errorView.message = viewModel.message
     }
     
     @IBAction private func refresh() {
