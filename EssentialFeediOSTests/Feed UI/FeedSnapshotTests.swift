98988e02099a0b186de0c886e9e593f8e6414ab2
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-03-27 18:01:01 +0100

    Load more items on will display LoadMoreCell

--- EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -83,23 +83,23 @@ class FeedSnapshotTests: XCTestCase {
     }
     
     private func feedWithLoadMoreIndicator() -> [CellController] {
-        let loadMore = LoadMoreCellController()
-            loadMore.display(ResourceLoadingViewModel(isLoading: true))
-            return feedWith(loadMore: loadMore)
-        }
-
-        private func feedWithLoadMoreError() -> [CellController] {
-            let loadMore = LoadMoreCellController()
-            loadMore.display(ResourceErrorViewModel(message: "This is a multiline\nerror message"))
-            return feedWith(loadMore: loadMore)
-        }
-
-        private func feedWith(loadMore: LoadMoreCellController) -> [CellController] {
+        let loadMore = LoadMoreCellController(callback: {})
+        loadMore.display(ResourceLoadingViewModel(isLoading: true))
+        return feedWith(loadMore: loadMore)
+    }
+    
+    private func feedWithLoadMoreError() -> [CellController] {
+        let loadMore = LoadMoreCellController(callback: {})
+        loadMore.display(ResourceErrorViewModel(message: "This is a multiline\nerror message"))
+        return feedWith(loadMore: loadMore)
+    }
+    
+    private func feedWith(loadMore: LoadMoreCellController) -> [CellController] {
         let stub = feedWithContent().last!
         let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
         stub.controller = cellController
         
-        let loadMore = LoadMoreCellController()
+        let loadMore = LoadMoreCellController(callback: {})
         loadMore.display(ResourceLoadingViewModel(isLoading: true))
         return [
             CellController(id: UUID(), cellController),
